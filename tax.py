74ffc8b7b181678e16a4f382a1430a752e1adc8f
abdalla-atalla <60201830+abdalla-atalla@users.noreply.github.com>
2020-08-01 10:08:32 -0700

    Update tax.py

--- tax.py
+++ tax.py
@@ -13,30 +13,27 @@ class TaxCalculator(EasyFrame):
         """Sets up the window and the widgets."""
         EasyFrame.__init__(self, title="Tax Calculator")
 
-        # Label and field for the income
-        # (self.incomeField)
+     
         self.addLabel(text="Gross income", row=0, column=0)
         self.incomeField = self.addFloatField(
             value=0.0, row=0, column=1, width=15)
 
-        # Label and field for the number of dependents
-        # (self.depField)
+     
         self.addLabel(text="Dependents", row=1, column=0)
         self.depField = self.addIntegerField(
             value=0, row=1, column=1, width=10)
 
-        # The command button
+     
         self.addButton(text="Compute", row=2, column=1,
                        columnspan=1,
                        command=self.computeTax)
 
-        # Label and field for the tax
-        # (self.taxField)
+      
         self.addLabel(text="Total Tax", row=3, column=0)
         self.taxField = self.addFloatField(
             value=0.0, row=3, column=1, width=15)
 
-    # The event handler method for the button
+  
     def computeTax(self):
         """Obtains the data from the input field and uses
         them to compute the tax, which is sent to the
